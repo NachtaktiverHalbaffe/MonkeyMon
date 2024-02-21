9f903396aec9c6aef1333c985a062223d57b0fc6
Felix Brugger <LeMonkay.VTIT@tutanota.com>
2024-02-21 11:23:55 +0100

    added mondex detail screen

---
+++ lib/src/presentation/screens/mondex_detail_screen.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:flutter_material_design_icons/flutter_material_design_icons.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:monkey_mon/src/domain/model/monkey_dto.dart';
+import 'package:monkey_mon/src/domain/usecases/fight_in_arena.dart';
+import 'package:monkey_mon/src/presentation/widgets/monkey_edit_modal_bottomsheet.dart';
+import 'package:monkey_mon/src/presentation/widgets/monkey_entry.dart';
+
+class MondexEntryScreen extends ConsumerWidget {
+  final MonkeyDto monkeyDto;
+  const MondexEntryScreen({super.key, required this.monkeyDto});
+
+  final double buttonPadding = 12;
+  final double buttonSpace = 12;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+            MonkeyEntry(monkeyDto: monkeyDto),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: buttonPadding),
+              child: OutlinedButton(
+                  onPressed: () {
+                    final arena = ref.read(
+                        arenaFightProvider(const Duration(seconds: 1))
+                            .notifier);
+                    arena.setFighter(monkeyDto: monkeyDto);
+                  },
+                  child: const ListTile(
+                      leading: Icon(MdiIcons.pokemonGo),
+                      title: Text("Als Kämpfer in die Arena schicken"))),
+            ),
+            SizedBox(height: buttonSpace),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: buttonPadding),
+              child: OutlinedButton(
+                  onPressed: () {
+                    final arena = ref.read(
+                        arenaFightProvider(const Duration(seconds: 1))
+                            .notifier);
+                    arena.setOpponent(monkeyDto: monkeyDto);
+                  },
+                  child: const ListTile(
+                      leading: Icon(MdiIcons.pokemonGo),
+                      title: Text("Als Gegner in die Arena schicken"))),
+            ),
+            SizedBox(height: buttonSpace),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: buttonPadding),
+              child: OutlinedButton(
+                  onPressed: () async {
+                    editMonkeyModalBottomSheet(
+                        context: context, initialData: monkeyDto);
+                  },
+                  child: const ListTile(
+                      leading: Icon(MdiIcons.pen),
+                      title: Text("Eintrag bearbeiten"))),
+            ),
+            SizedBox(height: buttonSpace),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: buttonPadding),
+              child: OutlinedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const ListTile(
+                      leading: Icon(MdiIcons.pokeball),
+                      title: Text("Zurück zum Pokedex"))),
+            ),
+            SizedBox(height: buttonSpace),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/presentation/widgets/monkey_edit_modal_bottomsheet.dart
@@ -0,0 +1,297 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:monkey_mon/src/domain/model/monkey_dto.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<MonkeyDto>? editMonkeyModalBottomSheet(
+    {required BuildContext context, MonkeyDto? initialData}) async {
+  TextEditingController nameTextController =
+      TextEditingController(text: initialData?.name);
+  TextEditingController descriptionTextController =
+      TextEditingController(text: initialData?.description);
+  TextEditingController speciesTextController =
+      TextEditingController(text: initialData?.species?.name);
+  TextEditingController knownFromTextController =
+      TextEditingController(text: initialData?.knownFrom);
+  TextEditingController strengthController =
+      TextEditingController(text: initialData?.strength);
+  TextEditingController weaknessesController =
+      TextEditingController(text: initialData?.weaknesses);
+  TextEditingController hpController =
+      TextEditingController(text: initialData?.hp.toString());
+  TextEditingController attackController =
+      TextEditingController(text: initialData?.attack.toString());
+  TextEditingController defenseController =
+      TextEditingController(text: initialData?.defense.toString());
+  TextEditingController specialAttackController =
+      TextEditingController(text: initialData?.specialAttack.toString());
+  TextEditingController specialDefenseController =
+      TextEditingController(text: initialData?.specialDefense.toString());
+  TextEditingController speedController =
+      TextEditingController(text: initialData?.speed.toString());
+  ImagePicker imagePicker = ImagePicker();
+
+  const double height = 20;
+  const String placeholderImage =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/0.png";
+  final double imageHeight = MediaQuery.of(context).size.width * 0.2;
+
+  MonkeyDto updatedData = initialData ?? const MonkeyDto(id: -1, name: "");
+  File? pickedImage;
+
+  await showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    showDragHandle: true,
+    useSafeArea: true,
+    builder: (BuildContext context) {
+      return SingleChildScrollView(
+        padding: EdgeInsets.only(
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom + height),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            GestureDetector(
+              onTap: () async {
+                var status = await Permission.mediaLibrary.status;
+                if (status.isDenied) {
+                  await Permission.mediaLibrary.request();
+                }
+                if (await Permission.location.isRestricted) {
+                  return;
+                }
+                final image =
+                    await imagePicker.pickImage(source: ImageSource.gallery);
+
+                if (image == null) return;
+                pickedImage = File(image.path);
+              },
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: imageHeight,
+                child: pickedImage == null
+                    ? CachedNetworkImage(
+                        height: imageHeight,
+                        imageUrl: initialData?.image ?? placeholderImage,
+                        fit: BoxFit.cover,
+                      )
+                    : Image.file(pickedImage!),
+              ),
+            ),
+            const SizedBox(height: height),
+            _textField(
+                context: context,
+                controller: nameTextController,
+                onChanged: (value) {
+                  nameTextController.text = value;
+                  updatedData = updatedData.copyWith(name: value);
+                },
+                labelText: "Name",
+                helperText: "Name des Affen eingeben"),
+            const SizedBox(height: height),
+            _textField(
+                context: context,
+                controller: descriptionTextController,
+                onChanged: (value) {
+                  descriptionTextController.text = value;
+                  updatedData = updatedData.copyWith(description: value);
+                },
+                labelText: "Beschreibungstext",
+                helperText: "Beschreibenden Text zum Affen eingeben"),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: speciesTextController,
+              onChanged: (value) {
+                speciesTextController.text = value;
+                updatedData = updatedData.copyWith(
+                    species: updatedData.species?.copyWith(name: value));
+              },
+              labelText: "Name der Affenspezies",
+              helperText: "Namen der Affenspezies eingeben",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: knownFromTextController,
+              onChanged: (value) {
+                knownFromTextController.text = value;
+                updatedData = updatedData.copyWith(knownFrom: value);
+              },
+              labelText: "Bekannt aus",
+              helperText: "Eingeben, woher (z.B. Film) der Affe bekannt ist",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: strengthController,
+              onChanged: (value) {
+                strengthController.text = value;
+                updatedData = updatedData.copyWith(strength: value);
+              },
+              labelText: "Stärken",
+              helperText: "Eingeben, welche Stärken der Affe hat",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: weaknessesController,
+              onChanged: (value) {
+                weaknessesController.text = value;
+                updatedData = updatedData.copyWith(weaknesses: value);
+              },
+              labelText: "Schwächen",
+              helperText: "Eingeben, welche Schwächen der Affe hat",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: hpController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(signed: false),
+              onChanged: (value) {
+                hpController.text = value;
+                if (value.isNotEmpty) {
+                  updatedData =
+                      updatedData.copyWith(hp: int.tryParse(value) ?? 0);
+                }
+              },
+              labelText: "KP",
+              helperText:
+                  "KP-Wert des Affen eingeben. Wichtig für den Arenakampf",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: attackController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(signed: false),
+              onChanged: (value) {
+                attackController.text = value;
+                if (value.isNotEmpty) {
+                  updatedData =
+                      updatedData.copyWith(attack: int.tryParse(value) ?? 0);
+                }
+              },
+              labelText: "Angriff",
+              helperText:
+                  "Angriffs-Wert des Affen eingeben. Wichtig für den Arenakampf",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: defenseController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(signed: false),
+              onChanged: (value) {
+                defenseController.text = value;
+                if (value.isNotEmpty) {
+                  updatedData =
+                      updatedData.copyWith(defense: int.tryParse(value) ?? 0);
+                }
+              },
+              labelText: "Verteidigung",
+              helperText:
+                  "Verteidungungs-Wert des Affen eingeben. Wichtig für den Arenakampf",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: specialAttackController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(signed: false),
+              onChanged: (value) {
+                specialAttackController.text = value;
+                if (value.isNotEmpty) {
+                  updatedData = updatedData.copyWith(
+                      specialAttack: int.tryParse(value) ?? 0);
+                }
+              },
+              labelText: "Spezial Angriff",
+              helperText:
+                  "Spezial-Angriffs-Wert des Affen eingeben. Wichtig für den Arenakampf",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: specialDefenseController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(signed: false),
+              onChanged: (value) {
+                specialDefenseController.text = value;
+                if (value.isNotEmpty) {
+                  updatedData = updatedData.copyWith(
+                      specialDefense: int.tryParse(value) ?? 0);
+                }
+              },
+              labelText: "Spezial Verteidigung",
+              helperText:
+                  "Spezial-Verteidungs-Wert des Affen eingeben. Wichtig für den Arenakampf",
+            ),
+            const SizedBox(height: height),
+            _textField(
+              context: context,
+              controller: speedController,
+              keyboardType:
+                  const TextInputType.numberWithOptions(signed: false),
+              onChanged: (value) {
+                speedController.text = value;
+                if (value.isNotEmpty) {
+                  updatedData =
+                      updatedData.copyWith(speed: int.tryParse(value) ?? 0);
+                }
+              },
+              labelText: "Initiative",
+              helperText:
+                  "Initiative-Wert des Affen eingeben. Wichtig für den Arenakampf",
+            ),
+            const SizedBox(height: height),
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Bestätigen"),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+
+  return updatedData;
+}
+
+TextField _textField(
+    {required BuildContext context,
+    required TextEditingController controller,
+    required Function(String) onChanged,
+    TextInputType? keyboardType = TextInputType.text,
+    String? labelText,
+    String? helperText,
+    Icon? icon}) {
+  return TextField(
+    controller: controller,
+    keyboardType: keyboardType,
+    onChanged: onChanged,
+    decoration: InputDecoration(
+      icon: icon,
+      floatingLabelStyle:
+          TextStyle(color: Theme.of(context).colorScheme.secondary),
+      border: const OutlineInputBorder(),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+          width: 2.0,
+        ),
+      ),
+      labelText: labelText,
+      labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+      helperText: helperText,
+    ),
+  );
+}
